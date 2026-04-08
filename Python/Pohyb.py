9cc997e63b55b1b1c0983246cf9b8369304ccb5b
BestaRadek <bestikradek@gmail.com>
2026-04-08 11:04:08 +0200

    srgadfb

--- Python/Pohyb.py
+++ Python/Pohyb.py
@@ -1,10 +1,6 @@
 x = 5
 y = 5
-<<<<<<< HEAD
 # Počáteční pozice
-=======
-# Počát
->>>>>>> efbeae4 (hiugz)
 
 while True:
     print("Player position:", x, y)
@@ -12,15 +8,9 @@ while True:
 
 # Posunutí o jeden pixel
     if move == "w":
-<<<<<<< HEAD
         y = y - 1
     elif move == "s":
         y = y + 1
-=======
-        y = y + 1
-    elif move == "s":
-        y = y - 1
->>>>>>> efbeae4 (hiugz)
     elif move == "a":
         x = x - 1
     elif move == "d":
